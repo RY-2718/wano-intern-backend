5d43cc3c609956a529ebe27c4193d05b424c5037
Ryo Yoshitani <ry.3607x5531@gmail.com>
2017-08-09 15:03:32 +0900

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  namespace :api do
+    # localhost:3000/api/v1/auth に認証API
+    mount_devise_token_auth_for 'User', at: '/v1/auth'
+
+    mount API::Root => '/' #この行は下記の Grape の実装後に追加
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
